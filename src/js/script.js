44d81a9e7a5710b06418d592b8d68d89c5887fc9
Diana <resrost@gmail.com>
2021-04-11 15:47:41 +0300

    correct

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
 // СЛАЙДЕР
 
     $('.first__slider').slick({
-      dots: true
+      dots: true,
+      autoplay: true,
+      infinite: true,
+      speed: 300
     });
 });
